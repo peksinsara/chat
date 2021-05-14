17f3b014a073e177527548009878ea32993d633a
Sara Peksin <peksin.sara@gmail.com>
2021-05-14 23:47:35 +0200

    message layout

--- src/components/MyMessage.js
+++ src/components/MyMessage.js
@@ -1,9 +1,19 @@
 import React from 'react'
 
-export default function MyMessage() {
+export default function MyMessage({ message }) {
+    /*image */
+    if (message?.attachments?.lenght > 0) {
+        return(
+            <img src={message.attachments[0].file}
+                alt="message-attachment"
+                className="message-image"
+                style={{float:'right'}}/>
+        )
+    }
     return (
-        <div>
-            
+        <div className="message" style={{floar:'right', marginRight: '18px', color:'white', backgroundColor:'#3B2A50'}}>
+            {/*display image */}
+            {message.text}
         </div>
     )
 }
